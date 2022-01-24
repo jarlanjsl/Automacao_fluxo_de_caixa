ba8e82b5fd5f8f175f436c690e5f350350e6005e
Jarlan <silvajarlan@gmail.com>
2022-01-24 08:26:11 -0300

    Modificando para 5 bkps

--- bkpFiles.py
+++ bkpFiles.py
@@ -1,6 +1,14 @@
 import shutil
 
-original = r'pythonsqlite.db'
+backup04 = r'.\bkp\pythonsqlite_bkp_old4.db'
+backup03 = r'.\bkp\pythonsqlite_bkp_old3.db'
+backup02 = r'.\bkp\pythonsqlite_bkp_old2.db'
+backup01 = r'.\bkp\pythonsqlite_bkp_old.db'
 target = r'.\bkp\pythonsqlite_bkp.db'
+original = r'pythonsqlite.db'
 
+shutil.copyfile(backup03, backup04)
+shutil.copyfile(backup02, backup03)
+shutil.copyfile(backup01, backup02)
+shutil.copyfile(target, backup01)
 shutil.copyfile(original, target)
