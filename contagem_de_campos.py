d58233ef14878fb6bd1b1e7a22c6b4c463470cbb
Jarlan <silvajarlan@gmail.com>
2022-02-10 16:58:04 -0300

    adaptacao para para tabela de movimentacao

--- contagem_de_campos.py
+++ contagem_de_campos.py
@@ -1,6 +1,6 @@
 import xlrd
 
-loc = 'novasEmpresas.xls'
+loc = 'orcado.xls'
 
 wb = xlrd.open_workbook(loc)
 sheet = wb.sheet_by_index(0)
@@ -19,22 +19,37 @@ l1 = 0
 ca = 0
 cb = 1
 cc = 2
-ce = 4
+cd = 3
 cf = 5
+ch = 7
+cj = 9
+ck = 10
+cm = 12
+cn = 13
 campo_a = sheet.cell_value(l1, ca)
 campo_b = sheet.cell_value(l1, cb)
 campo_c = sheet.cell_value(l1, cc)
-campo_e = sheet.cell_value(l1, ce)
+campo_d = sheet.cell_value(l1, cd)
 campo_f = sheet.cell_value(l1, cf)
+campo_h = sheet.cell_value(l1, ch)
+campo_j = sheet.cell_value(l1, cj)
+campo_k = sheet.cell_value(l1, ck)
+campo_m = sheet.cell_value(l1, cm)
+campo_n = sheet.cell_value(l1, cn)
 
 while linhas > 0:
     print(f'Linhas restantes: {linhas - 1}')
     print(f'Linha atual: {l1}')
-    print(f"Linha {l1} = '{campo_a}','{campo_b}','{campo_c}','{campo_e}','{campo_f}'")
+    print(f"Linha {l1} = '{campo_a}','{campo_b}','{campo_c}','{campo_d}','{campo_f}','{campo_h}','{campo_j}','{campo_k},'{campo_m},'{campo_n}'")
     linhas -= 1
     l1 += 1
     campo_a = sheet.cell_value(l1, ca)
     campo_b = sheet.cell_value(l1, cb)
     campo_c = sheet.cell_value(l1, cc)
-    campo_e = sheet.cell_value(l1, ce)
+    campo_d = sheet.cell_value(l1, cd)
     campo_f = sheet.cell_value(l1, cf)
+    campo_h = sheet.cell_value(l1, ch)
+    campo_j = sheet.cell_value(l1, cj)
+    campo_k = sheet.cell_value(l1, ck)
+    campo_m = sheet.cell_value(l1, cm)
+    campo_n = sheet.cell_value(l1, cn)
--- insert_por_linha.py
+++ insert_por_linha.py
@@ -2,7 +2,7 @@ import xlrd
 import sqlite3
 from sqlite3 import Error
 
-loc = 'novasEmpresas.xls'
+loc = 'orcado.xls'
 
 wb = xlrd.open_workbook(loc)
 sheet = wb.sheet_by_index(0)
@@ -31,15 +31,15 @@ def create_empresas(conn, empresas):
     :param empresas:
     :return:
     """
-    sql = ''' INSERT INTO empresas(id_empresa,descricao,usuario_criador,dt_criacao,status)
-              VALUES(?,?,?,?,?) '''
+    sql = ''' INSERT INTO movimentacao(id_mov, id_empresa, descricao, id_conta, id_fornecedor, dt_documento, dt_vencimento, valor_orcado, usuario_criador, status)
+              VALUES(?,?,?,?,?,?,?,?,?,?) '''
     cur = conn.cursor()
     cur.execute(sql, empresas)
     conn.commit()
     return cur.lastrowid
 
 
-loc = 'novasEmpresas.xls'
+loc = 'orcado.xls'
 
 wb = xlrd.open_workbook(loc)
 sheet = wb.sheet_by_index(0)
@@ -55,23 +55,39 @@ l1 = 1
 ca = 0
 cb = 1
 cc = 2
-ce = 4
+cd = 3
 cf = 5
+ch = 7
+cj = 9
+ck = 10
+cm = 12
+cn = 13
 campo_a = sheet.cell_value(l1, ca)
 campo_b = sheet.cell_value(l1, cb)
 campo_c = sheet.cell_value(l1, cc)
-campo_e = sheet.cell_value(l1, ce)
+campo_d = sheet.cell_value(l1, cd)
 campo_f = sheet.cell_value(l1, cf)
+campo_h = sheet.cell_value(l1, ch)
+campo_j = sheet.cell_value(l1, cj)
+campo_k = sheet.cell_value(l1, ck)
+campo_m = sheet.cell_value(l1, cm)
+campo_n = sheet.cell_value(l1, cn)
 
 while linhas > 0:
-    print(f"'{campo_a}','{campo_b}','{campo_c}','{campo_e}','{campo_f}'")
+    print(
+        f"Linha {l1} = '{campo_a}','{campo_b}','{campo_c}','{campo_d}','{campo_f}','{campo_h}','{campo_j}','{campo_k},'{campo_m},'{campo_n}'")
     linhas -= 1
     l1 += 1
     campo_a = sheet.cell_value(l1, ca)
     campo_b = sheet.cell_value(l1, cb)
     campo_c = sheet.cell_value(l1, cc)
-    campo_e = sheet.cell_value(l1, ce)
+    campo_d = sheet.cell_value(l1, cd)
     campo_f = sheet.cell_value(l1, cf)
+    campo_h = sheet.cell_value(l1, ch)
+    campo_j = sheet.cell_value(l1, cj)
+    campo_k = sheet.cell_value(l1, ck)
+    campo_m = sheet.cell_value(l1, cm)
+    campo_n = sheet.cell_value(l1, cn)
 
 
     def main():
@@ -81,7 +97,7 @@ while linhas > 0:
         conn = create_connection(database)
         with conn:
             # create a new empresas
-            empresas = (campo_a, campo_b, campo_c, campo_e, campo_f)
+            empresas = (campo_a, campo_b, campo_c, campo_d, campo_f, campo_h, campo_j, campo_k, campo_m, campo_n)
             create_empresas(conn, empresas)
 
 
